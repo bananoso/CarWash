b356098ab2cf151d3cc2b25bfe20db289609aaf2
Student <>
2018-12-14 11:15:24 +0200

    ObservableObject => Add class; Observer, ObserverCollection => Move to ObservableObject extension

---
+++ Sources/Entities/ObservableObject.swift
@@ -0,0 +1,70 @@
+//
+//  ObservableObject.swift
+//  CarWashProject
+//
+//  Created by Student on 14.12.2018.
+//  Copyright © 2018 IDAP. All rights reserved.
+//
+
+import Foundation
+
+class ObservableObject<Value> {
+    
+    typealias Handler = (Value) -> ()
+    
+    private let observers = Observers()
+    
+    func observer(value: Value, handler: @escaping Handler) -> Observer {
+        let observer = Observer(sender: self, handler: handler)
+        self.observers.add(observer)
+        observer.handler(value)
+        
+        return observer
+    }
+}
+
+extension ObservableObject {
+    
+    class Observer: Hashable {
+        
+        public var isObserving: Bool {
+            return self.sender != nil
+        }
+        
+        public var hashValue: Int {
+            return ObjectIdentifier(self).hashValue
+        }
+        
+        weak private(set) var sender: AnyObject?
+        let handler: Handler
+        
+        init(sender: AnyObject, handler: @escaping Handler) {
+            self.sender = sender
+            self.handler = handler
+        }
+        
+        func cancel() {
+            self.sender = nil
+        }
+        
+        static func == (lhs: Observer, rhs: Observer) -> Bool {
+            return lhs === rhs
+        }
+    }
+    
+    class Observers {
+        
+        private let observers = Atomic([Observer]())
+        
+        func add(_ observer: Observer) {
+            self.observers.modify { $0.append(observer) }
+        }
+        
+        func notify(value: Value) {
+            self.observers.modify {
+                $0 = $0.filter { $0.isObserving }
+                $0.forEach { $0.handler(value) }
+            }
+        }
+    }
+}
