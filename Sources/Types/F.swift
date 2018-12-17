8d895bd051437c9aa0868d3b7ee2312366d60679
Student <>
2018-12-17 14:46:37 +0200

    F => Add func weakify

--- Sources/Types/F.swift
+++ Sources/Types/F.swift
@@ -28,3 +28,15 @@ func when<Result>(_ condition: Bool, execute: () -> Result) -> Result? {
 func unless<Result>(_ condition: Bool, execute: () -> Result) -> Result? {
     return when(!condition, execute: execute)
 }
+
+@discardableResult
+func weakify<Wrapped: AnyObject>(_ value: Wrapped, execute: (Weak<Wrapped>) -> ()) -> Weak<Wrapped> {
+    let weak = Weak(value)
+    execute(weak)
+    
+    return weak
+}
+
+func weakify<Wrapped: AnyObject>(_ value: Wrapped) -> Weak<Wrapped> {
+    return weakify(value) {_ in }
+}
